8a8b3293e8214418b86beb9e8832fa45136a21b6
Amar Hamdani <amar.hamdani@gmail.com>
2022-09-20 00:48:13 +0700

    "Melakukan promise pada file promise.js"

--- promise.js
+++ promise.js
@@ -1,8 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (hasilNonton) => {
+  let jumlah = 0;
+  return new Promise(async (resolve,rejected) => {
+    let x = await  promiseTheaterIXX()
+        .then((data) => {
+          data.forEach(element => {
+            if(element.hasil===hasilNonton){
+              jumlah++;
+            }
+          });
+      });
+      
+    let y = await  promiseTheaterVGC()
+        .then((data) => {
+          data.forEach(element => {
+            if(element.hasil===hasilNonton){
+              jumlah++;
+            }
+          });
+      });
 
+    resolve(jumlah)
+    
+  });
+};
 module.exports = {
   promiseOutput,
 };
